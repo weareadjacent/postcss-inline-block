8142db9a3851b0097b93262da4ea99a7301057e2
Tom McCaffery <tom@weareadjacent.com>
2019-11-12 08:23:26 -0500

    Change order of rules

--- index.js
+++ index.js
@@ -5,24 +5,12 @@ module.exports = postcss.plugin('postcss-inline-block', function () {
     // Work with options here
     function inlineBlockFix(decl){
     	var origRule = decl.parent;
-    	origRule.append(
-            {
-                prop: 'vertical-align',
-                value: 'middle'
-            },
-            {
-                prop: '*vertical-align',
-                value: 'auto'
-            },
-            {
-                prop: '*zoom',
-                value: '1'
-            },
-            {
-                prop: '*display',
-                value: 'inline'
-            }
-    	);
+        origRule.insertAfter(decl, [
+            postcss.decl({prop: 'vertical-align', value: 'middle'}),
+            postcss.decl({prop: '*vertical-align', value: 'auto'}),
+            postcss.decl({prop: '*zoom', value: '1'}),
+            postcss.decl({prop: '*display', value: 'inline'})
+        ]);
     }
 
     return function (css) {
